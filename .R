3e5a25aff9b305eaab364d276a9542d5d3892297
Rongeet <64375640+Rongeet@users.noreply.github.com>
2020-05-02 13:59:10 +0530

    Update .R

--- .R
+++ .R
@@ -23,8 +23,7 @@ dev.off()
     lines(Sub_metering_2~dateTime,col='Red')
     lines(Sub_metering_3~dateTime,col='Blue')
   })
-##plot 3
-  legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
+legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
          c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
