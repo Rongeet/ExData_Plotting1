9a04846770fff72668e29934e7446b4662564bbe
Rongeet <64375640+Rongeet@users.noreply.github.com>
2020-05-02 13:58:04 +0530

    Update .R

--- .R
+++ .R
@@ -8,25 +8,27 @@ dateTime <- setNames(dateTime, "DateTime")
 t <- t[ ,!(names(t) %in% c("Date","Time"))]
 t <- cbind(dateTime, t)
 t$dateTime <- as.POSIXct(dateTime)
+##plot 1
   hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
 dev.copy(png,"plot1.png", width=480, height=480)
 dev.off()
+##plot 2
 plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 dev.copy(png,"plot2.png", width=480, height=480)
 dev.off()
+##plot 3
  with(t, {
     plot(Sub_metering_1~dateTime, type="l",
          ylab="Global Active Power (kilowatts)", xlab="")
     lines(Sub_metering_2~dateTime,col='Red')
     lines(Sub_metering_3~dateTime,col='Blue')
   })
+##plot 3
   legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
          c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-Saving to file
-  #dev.copy(png, file="plot3.png", height=480, width=480)
-  #dev.off()
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
+##plot 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
   with(t, {
     plot(Global_active_power~dateTime, type="l", 
